ae5e0595867b237819117c48fa9cbe4462e563dc
mooonaaa1213 <monaskiing57@gmail.com>
2025-06-12 10:42:29 +0900

    プロジェクト作成

---
+++ src/kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		Car_Chapter15 c = new Car_Chapter15();
+		c.gearChange(3);
+		c.run();
+
+	}
+
+}
